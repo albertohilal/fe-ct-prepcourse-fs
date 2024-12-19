cecc3ae2de3f18092b48db33a054904176b2fa97
albertohilal <albertohilal@github.com>
2024-12-19 09:02:51 -0300

    Resueltos ejercicios de C5 Fundamentos Javascript/ejercicios del 12.js al 20.js

--- C5 Fundamentos Javascript/ejercicios/19.js
+++ C5 Fundamentos Javascript/ejercicios/19.js
@@ -1,10 +1,14 @@
 function suma(x, y) {
   // La función recibe dos argumentos llamados "x" e "y" que son números.
   // Retorna el resultado de la suma de estos argumentos.
-  // Por ejemplo: 
+  // Por ejemplo:
   // 5, 5 ---> 10
   // -5, 5 ---> 0
   // Tu código:
+  if (typeof x !== "number" || typeof y !== "number") {
+    return "Error: Ambos argumentos deben ser números";
+  }
+  return x + y;
 }
 
-module.exports = suma; 
+module.exports = suma;
